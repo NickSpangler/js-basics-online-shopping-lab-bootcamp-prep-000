6ee5ec0e46d0b1e1a2725e2134cd3d8a772b7536
Nick Spangler <spangleyman@mac.com>
2020-05-18 02:53:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,9 +56,6 @@ function removeFromCart(item) {
       cart.splice(i, 1)
       return cart
     } 
-    if (cart[i]["itemName"] !== item) {
-      return "That item is not in your cart."
-    }
   }
 }
 
