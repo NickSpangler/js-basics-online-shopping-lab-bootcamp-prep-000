3759e86e4bfa8940d66048081fcb71cc1364e5af
Nick Spangler <spangleyman@mac.com>
2020-05-18 03:07:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,6 +59,6 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber === "") {
-    return "Sorry, we don't have a credit card on file for you."
+    document.write("Sorry, we don't have a credit card on file for you.")
   }
 }
