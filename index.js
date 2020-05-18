5ccad774855c26f4a7c0a8af0b984897dfed4484
Nick Spangler <spangleyman@mac.com>
2020-05-18 02:41:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,9 +54,9 @@ function removeFromCart(item) {
   for (i = 0; i < cart.length; i++) {
     if(cart[i]["itemName"] === item) {
       cart.splice(i, 1)
+      return cart
     }
   }
-  return cart
 }
 
 function placeOrder(cardNumber) {
